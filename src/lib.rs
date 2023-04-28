042a0df3eae49a3497b61f8e08d8eee5c8c9c1ae
EnricoPDG <ricopapsch@hotmail.com>
2023-04-28 14:42:32 -0300

    project

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+wai_bindgen_rust::export!("calculator.wai");
+
+struct Calculator;
+
+impl crate::calculator::Calculator for Calculator {
+    fn add(a: f32, b: f32) -> f32 {
+        return a + b;
+    }
+}
